0f6d01e8711cb722cd36434fd7341b927d47360e
Jake <jakel@protonmail.com>
2017-12-03 00:00:48 +0100

    Comment out Windows TimerResolution change

--- LaserGRBL/Tools/TaskScheduler.cs
+++ LaserGRBL/Tools/TaskScheduler.cs
@@ -9,7 +9,7 @@ namespace Tools
 	public class TaskScheduler
 	{
 
-		[System.Runtime.InteropServices.DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+		//[System.Runtime.InteropServices.DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
 		private static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
 
 		public static void SetClockResolution(int msec) //volendo potrebbe gestire anche 0.5 msec ma a noi basta 1-15 msec, quindi usiamo int
@@ -17,7 +17,7 @@ namespace Tools
 			uint DesiredResolution = (uint)msec * 10000;
 			bool SetResolution = true;
 			uint CurrentResolution = 0;
-			NtSetTimerResolution(DesiredResolution, SetResolution, ref CurrentResolution);
+			//NtSetTimerResolution(DesiredResolution, SetResolution, ref CurrentResolution);
 		}
 
 	}
